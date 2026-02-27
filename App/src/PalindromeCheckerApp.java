b80dcadcaa6aba6e36ec00b3148fa70faee8ac5f
ayu-333 <ayushanil9747@gmail.com>
2026-02-27 10:46:31 +0530

    UC9 completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,56 +1,13 @@
-class Node {
-    char data;
-    Node next;
-    Node(char data) { this.data = data; }
-}
-
 public class PalindromeCheckerApp {
 
-    // Convert string to linked list
-    private static Node stringToLinkedList(String s) {
-        Node head = null, tail = null;
-        for (char c : s.toLowerCase().toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        return head;
-    }
-
-    private static Node reverseList(Node head) {
-        Node prev = null, curr = head;
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
     public static boolean isPalindrome(String text) {
-        Node head = stringToLinkedList(text);
+        text = text.toLowerCase();
 
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+        if (text.length() <= 1) return true;
 
-        Node secondHalf = reverseList(slow);
+        if (text.charAt(0) != text.charAt(text.length() - 1)) return false;
 
-        Node firstHalf = head;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) return false;
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+        return isPalindrome(text.substring(1, text.length() - 1));
     }
 
     public static void main(String[] args) {
